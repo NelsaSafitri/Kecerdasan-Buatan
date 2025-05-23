0bcdf5233b5cc987839a95dff1c780d39b5d031d
Nelsa Safitri <146648667+NelsaSafitri@users.noreply.github.com>
2025-05-23 09:34:06 +0700

    Add files via upload

---
+++ nlp/simulasi.py
@@ -0,0 +1,7 @@
+
+nilai_siswa = int(input("Masukkan nilai siswa: "))
+
+if nilai_siswa >= 75:
+    print("Lulus")
+else:
+    print("Tidak Lulus")
---
+++ nlp/simulasi2.py
@@ -0,0 +1,3 @@
+numbers = [4, 12, 7, 23, 9]
+largest = max(numbers)
+print("The largest number is:", largest)
